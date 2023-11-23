8a14a9d74d8cf091bb9c049df14360ff20364c97
defnotnelli <151853275+defnotnelli@users.noreply.github.com>
2023-11-23 17:46:13 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -41,11 +41,11 @@ def car_racing():
     playerCar.rect.y = SCREENHEIGHT - 100
 
     car1 = Car("YeRari.png", 50, 75, random.randint(50, 100))
-    car1.rect.x = 275
+    car1.rect.x = 280
     car1.rect.y = -300
 
     car2 = Car("BlackRari.png", 50, 75, random.randint(50, 100))
-    car2.rect.x = 375
+    car2.rect.x = 380
     car2.rect.y = -600
 
     car3 = Car("BlueRari.png", 50, 75, random.randint(50, 100))
@@ -75,29 +75,38 @@ def car_racing():
         # screen
         screen.fill(BLACK)
 
-        text1 = cooler_font.render("Game Over", True, (255, 0, 0))
-        text_rect = text1.get_rect(center=(SCREENWIDTH // 2, SCREENHEIGHT // 2 - 50))
+        text1 = cooler_font.render("Game Over", True, RED)
+        text_rect = text1.get_rect(center=(SCREENWIDTH // 2, SCREENHEIGHT // 2 - 200))
         screen.blit(text1, text_rect)
 
         text2 = cooler_font.render(f"Score: {score}", True, WHITE)
-        text_rect = text2.get_rect(center=(SCREENWIDTH // 2, SCREENHEIGHT // 2 + 30))
+        text_rect = text2.get_rect(center=(SCREENWIDTH // 2, SCREENHEIGHT // 2 - 150))
         screen.blit(text2, text_rect)
 
-        lil_sass = cooler_font.render("Forti bu eh cabali", True, (255, 0, 0))
-        lil_sass_rect = lil_sass.get_rect(center=(SCREENWIDTH // 2, SCREENHEIGHT // 2 + 100))
+        lil_sass = cooler_font.render("Do better next time!", True, RED)
+        lil_sass_rect = lil_sass.get_rect(center=(SCREENWIDTH // 2, SCREENHEIGHT // 2 - 100))
         screen.blit(lil_sass, lil_sass_rect)
+
+        # Display difficulty level
+        final_level = cooler_font.render(f"Level: {int(level)}", True, RED)
+        final_level_rect = final_level.get_rect(center=(SCREENWIDTH // 2, SCREENHEIGHT // 2 + 200))
+        screen.blit(final_level, final_level_rect)
 
         pygame.display.flip()
         pygame.time.wait(1000)  # Display the game-over screen for  seconds
 
-    # Load the background image
+    # Load the images and sounds
     background_image = pygame.image.load("gameback.png").convert()
+    level_up_sound = pygame.mixer.Sound("levelUp.mp3")
+    game_over_sound = pygame.mixer.Sound("dun-dun.mp3")
+
+
 
     base_speed = 1  # Set your desired base speed
-
     traffic_car_speed = base_speed
     frame_counter = 0
     frames_to_increase_difficulty = 250
+    level = 0
 
     # Allowing the user to close the window...
     carryOn = True
@@ -124,16 +133,19 @@ def car_racing():
             speed += 0.03
             playerCar.moveForward(speed)
 
-        # Update the speed based on the difficulty level
-        speed = base_speed * difficulty_level
+        # Increment level every 250 frames
         frame_counter += 1
         if frame_counter >= frames_to_increase_difficulty:
             frame_counter = 0
-            difficulty_level += 0.2  # Increase difficulty level
+            level += 1  # Increment level
+            level_up_sound.play()  # Play the level-up sound
+
+        # Update the speed based on the level
+        traffic_car_speed = base_speed * (1 + 0.2 * level)
 
         # Game Logic
         for car in all_coming_cars:
-            car.moveForward(traffic_car_speed * difficulty_level)
+            car.moveForward(traffic_car_speed)
             if car.rect.y > SCREENHEIGHT:
                 car.changeSpeed(random.randint(50, 100))
                 print("Car reached the limit. Changing speed.")
@@ -148,18 +160,18 @@ def car_racing():
                     car.rect.x = 550  # Reset car4 to the rightmost lane
                     car.load_car_image(random.choice(imageList))  # Randomize the car image
 
-            # Update the speed based on the difficulty level
-            traffic_car_speed = base_speed * difficulty_level
 
-            for car in all_coming_cars:
-                if car.rect.y > SCREENHEIGHT:
-                    print("Changing car image")
-                    car.load_car_image(random.choice(imageList))
+
+        for car in all_coming_cars:
+            if car.rect.y > SCREENHEIGHT:
+                print("Changing car image")
+                car.load_car_image(random.choice(imageList))
 
             # Check if there is a car collision
             car_collision_list = pygame.sprite.spritecollide(playerCar, all_coming_cars, False)
             for car in car_collision_list:
                 print("Car crash with player!")
+                game_over_sound.play()  # Play the game_over sound
                 show_game_over_screen(screen, score)
                 # End Of Game
                 carryOn = False
@@ -183,8 +195,13 @@ def car_racing():
         pygame.draw.line(screen, WHITE, [550, 0], [550, SCREENHEIGHT], 5)
         pygame.draw.line(screen, WHITE, [650, 0], [650, SCREENHEIGHT], 5)
 
+        # Display the score
         text = cooler_font.render(f"Score: {score}", True, shade_of_purple)
         screen.blit(text, (10, 10))
+
+        # Display difficulty level
+        level_text = cooler_font.render(f"Level: {int(level)}", True, shade_of_purple)
+        screen.blit(level_text, (10, 40))
 
         # Now let's draw all the sprites in one go. (For now we only have 1 sprite!)
         all_sprites_list.draw(screen)
