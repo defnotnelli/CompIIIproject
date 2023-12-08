c1cc35d4dd4a11e74124a946efdcd1232c5eb358
defnotnelli <151853275+defnotnelli@users.noreply.github.com>
2023-12-08 23:47:36 +0000

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,12 +1,52 @@
+"""
+Game Interface
+
+This module defines the game interface and menu using the Pygame library. It provides options for the player, including starting the game, accessing instructions, viewing credits, and quitting the game.
+
+Usage:
+    - Run the 'interface' function to display the main menu.
+    - Click on buttons to navigate through different options.
+    - The 'Get Started' button starts the car racing game.
+    - The 'Multiplayer' button can be linked to a multiplayer function.
+    - The 'Instructions' button displays game instructions.
+    - The 'Credits' button displays credits for the game.
+    - The 'Quit' button exits the game.
+
+Functionality:
+    - Background music is played using the 'play_background_music' function.
+    - Interface elements include buttons with color-changing effects.
+    - The mouse is used to interact with the interface.
+
+
+"""
+
+
 import pygame
 import sys
-from game import car_racing
+import pygame.mixer
+from game4 import car_racing
+
+pygame.mixer.init()
+
+
+def play_background_music(volume=0.1):
+    # Load background music
+    background_music = pygame.mixer.Sound("backgroundTrack.mp3")
+
+    background_music.set_volume(volume)
+
+    # Start playing background music
+    background_music.play(-1)  # music will loop indefinitely
+
+def stop_background_music():
+    pygame.mixer.stop()
 
 
 # Creating a function that creates the GUI
 def interface():
-    # initiating pygames
+    # initiating pygame
     pygame.init()
+    play_background_music(volume=0.1)
     # creating the screen 720x720 pixels
     res = (720, 720)
     screen = pygame.display.set_mode(res)
@@ -14,7 +54,11 @@ def interface():
     # Load the background image
     background_image = pygame.image.load("interface.png").convert()
 
-    pygame.display.set_caption("Djogo di Carru")
+    # Load the icon image
+    pygame_icon = pygame.image.load("icon.png")
+    pygame.display.set_icon(pygame_icon)
+
+    pygame.display.set_caption("Let's go!")
 
     # creating some colors (RGB scale)
     yellow = (255, 255, 0)
@@ -23,7 +67,7 @@ def interface():
     blue = (0, 0, 255)
     color_light = (170, 170, 170)
     color_dark = (100, 100, 100)
-    black = (0, 0, 0)
+    BLACK = (0, 0, 0)
     shade_of_purple = (255, 0, 255)
     cyberpunk_blue = (99, 0, 255)
     blablue = (9, 24, 51)
@@ -32,7 +76,7 @@ def interface():
     width = screen.get_width()
     height = screen.get_height()
     # creating some textlabels
-    cooler_font = pygame.font.SysFont("Eight-Bit Madness", 50)
+    cooler_font = pygame.font.SysFont("Eight-Bit Madness", 45)
     game1_text = cooler_font.render('Get Started', True, shade_of_purple)
     game2_text = cooler_font.render('Multiplayer', True, shade_of_purple)
     game3_text = cooler_font.render('Instructions', True, shade_of_purple)
@@ -43,9 +87,9 @@ def interface():
     screen_width = 720
     screen_height = 720
     # Calculate the positions for the interface elements
-    button_width = 200
-    button_height = 60
-    button_margin = 20
+    button_width = 250
+    button_height = 65
+    button_margin = 25
     button_x = (screen_width - button_width) // 2
     game1_pos = (button_x, (screen_height - (4 * button_height + 3 * button_margin)) // 2)
     game2_pos = (button_x, game1_pos[1] + button_height + button_margin)
@@ -68,7 +112,7 @@ def interface():
 
                 # Multiplayer Button
                 # elif game2_pos[0] <= mouse[0] <= game2_pos[0] + button_width and game2_pos[1] <= mouse[1] <= game2_pos[1] + button_height:
-                #     multiplayer_function()
+                # multiplayer_function()
 
                 # Instructions Button
                 elif game3_pos[0] <= mouse[0] <= game3_pos[0] + button_width and game3_pos[1] <= mouse[1] <= game3_pos[
@@ -141,11 +185,11 @@ def interface():
         else:
             pygame.draw.rect(screen, blablue, [quit_pos[0], quit_pos[1], button_width, button_height])
 
+
         # Blit text on the screen
         screen.blit(quit_text, quit_pos)
 
-        # pygame.draw.rect(screen, color_dark, [52, 0, 612, 100])
-        # screen.blit(title_text, (55, 0))
+
         # PYGAME BUILT-IN FUNCTION that updates the screen at every iteration of the loop
         pygame.display.update()
 
@@ -171,6 +215,8 @@ def credits_():
             # press on exit button
             if ev.type == pygame.QUIT:
                 pygame.quit()
+                stop_background_music()
+
             # press on quit button
             if ev.type == pygame.MOUSEBUTTONDOWN:
                 if 450 <= mouse[0] <= 450 + 140 and 5 * 120 <= mouse[1] <= 5 * 120 + 60:
@@ -205,7 +251,6 @@ def instructions():
     cooler_font = pygame.font.SysFont("Eight-Bit Madness", 50)
     back_text = cooler_font.render('Back', True, shade_of_purple)
 
-
     while True:
         mouse = pygame.mouse.get_pos()
         for ev in pygame.event.get():
@@ -228,19 +273,4 @@ def instructions():
             pygame.draw.rect(screen, color_dark, [450, 5 * 120, 140, 60])
         screen.blit(back_text, (450, 5 * 120))
 
-
         pygame.display.update()
-
-
-if __name__ == "__main__":
-    interface()
-
-
-
-
-
-
-
-
-
-
